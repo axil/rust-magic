2bb2c1ffa669cf4989c36f8e6927d50dca6d81d5
Lev Maximov <lev.maximov@gmail.com>
2019-08-19 10:54:05 +0700

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rust_magic",
-    version="0.1.0",
+    version="0.2.0",
     author='Lev Maximov',
     author_email='lev.maximov@gmail.com',
     url='https://github.com/axil/rust-magic',
